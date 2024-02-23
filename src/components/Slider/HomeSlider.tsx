50b261609d0c452edaa860f3b8ff714d835ce9a7
prosper edafe <mamusedafe19@gmail.com>
2024-02-23 09:24:56 +0100

    home slider added

--- src/components/Slider/HomeSlider.tsx
+++ src/components/Slider/HomeSlider.tsx
@@ -1,26 +1,26 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, SwiperClass } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { EffectFade, Navigation, Pagination, Autoplay } from 'swiper/modules';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { slides } from '../../data/homeSlider';
 import { Link } from 'react-router-dom';
 
 export const HomeSlider: FC = () => {
-    // const [swiperRef, setSwiperRef] = useState<SwiperClass | null>(null);
+    const [swiperRef, setSwiperRef] = useState<SwiperClass | null>(null);
 
     return (
         <div className='home__swiper'>
             <div className="navigation-buttons">
-                <button>
+                <button onClick={() => swiperRef?.slidePrev()}>
                     <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect x="40.6204" y="40.6204" width="40.2407" height="40.2407" rx="1.62037" transform="rotate(-180 40.6204 40.6204)" stroke="#F8F8F8" strokeWidth="0.759259" />
                         <path d="M15.9742 20.7662L22.0184 14.3766L23.4287 15.8675L18.7948 20.7662L23.4287 25.6649L22.0184 27.1558L15.9742 20.7662Z" fill="#F8F8F8" />
                     </svg>
                 </button>
-                <button>
+                <button onClick={() => swiperRef?.slideNext()}>
                     <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect x="0.37963" y="0.37963" width="40.2407" height="40.2407" rx="1.62037" stroke="#F8F8F8" strokeWidth="0.759259" />
                         <path d="M25.0258 20.2338L18.9816 26.6234L17.5713 25.1325L22.2052 20.2338L17.5713 15.3351L18.9816 13.8442L25.0258 20.2338Z" fill="#F8F8F8" />
@@ -28,7 +28,7 @@ export const HomeSlider: FC = () => {
                 </button>
             </div>
             <Swiper
-                // onSwiper={setSwiperRef}
+                onSwiper={setSwiperRef}
                 effect={'fade'}
                 loop={true}
                 pagination={{
--- src/data/homeSlider.ts
+++ src/data/homeSlider.ts
@@ -1,6 +1,7 @@
-import slide1 from "../assets/Seacraft Asset/Images/steptodown.com342391.jpg.png"
-import slide2 from "../assets/Seacraft Asset/Images/steptodown.com662193.jpg.png"
-import slide3 from "../assets/Seacraft Asset/Images/steptodown.com807566.jpg.png"
+import slide1 from "../assets/Seacraft Asset/Images/slide1.jpeg"
+import slide2 from "../assets/Seacraft Asset/Images/steptodown.com342391.jpg.png"
+import slide3 from "../assets/Seacraft Asset/Images/steptodown.com662193.jpg.png"
+import slide4 from "../assets/Seacraft Asset/Images/steptodown.com807566.jpg.png"
 
 export const slides = [
     {
@@ -14,18 +15,18 @@ export const slides = [
         topic: "YEARS OF EXPERIENCE",
         name: "Growth, Adaptability & Excellence",
         description: "Over the years, we've honed our skills, refined our processes, and built a solid foundation of trust with our clients. With each project we undertake, we leverage our extensive experience to deliver innovative solutions, ensuring the success and satisfaction of our clients.",
-        background: slide1
+        background: slide2
     },
     {
         topic: "EXPERTISE",
         name: "Quality, Safety & Innovation",
         description: "At Seacraft, our expertise spans the entire spectrum of the marine and offshore industry, positioning us as leaders in providing innovative solutions and services to our clients worldwide.",
-        background: slide2
+        background: slide3
     },
     {
         topic: "SUSTAINABILITY",
         name: "Sustainability is not just a buzzword",
         description: "It's a core value that guides every aspect of our operations. and we are committed to minimizing our ecological footprint and promoting sustainable practices at every opportunity.",
-        background: slide3
+        background: slide4
     }
 ]
