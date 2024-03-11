c8ab57cebfb52f6a5961e43d9e67b838d02ef4b3
prosper edafe <mamusedafe19@gmail.com>
2024-03-11 11:53:04 +0100

    border alignment fixed

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,7 +1,31 @@
-import { FC } from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import { services } from "../data/services";
 
 const Services: FC = () => {
+    const elementRef = useRef<any>(null);
+    const [width, setWidth] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (elementRef.current) {
+                setWidth(elementRef.current.offsetWidth);
+            }
+        };
+
+        // Initial width calculation
+        if (elementRef.current) {
+            setWidth(elementRef.current.offsetWidth);
+        }
+
+        // Event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div id="services">
             <div className="wrapper">
@@ -13,8 +37,16 @@ const Services: FC = () => {
                 </h3>
                 <p>With a relentless pursuit of cutting-edge solutions, we leverage advanced technology to deliver groundbreaking services that set new standards in the marine industry, driving efficiency, safety, and excellence.</p>
                 <div className="services__type">
-                    {services.map((service: any, index: number) =>
-                        <figure key={index}>
+                    {services.slice(0, 12).map((service: any, index: number) =>
+                        <figure ref={elementRef} className="first" key={index}>
+                            <img src={service.logo} alt="" loading="lazy" />
+                            <figcaption>{service.name}</figcaption>
+                        </figure>
+                    )}
+                </div>
+                <div className="last">
+                    {services.slice(12).map((service: any, index: number) =>
+                        <figure style={{ maxWidth: width }} key={index}>
                             <img src={service.logo} alt="" loading="lazy" />
                             <figcaption>{service.name}</figcaption>
                         </figure>
